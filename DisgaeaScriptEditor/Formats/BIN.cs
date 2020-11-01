2194526613fa9893af391713e1698e989c1bda1e
Krisan Thyme <krisanthyme@gmail.com>
2020-11-01 07:25:53 -0500

    Additional Opcode support.

--- DisgaeaScriptEditor/Formats/BIN.cs
+++ DisgaeaScriptEditor/Formats/BIN.cs
@@ -139,6 +139,37 @@ namespace DisgaeaScriptEditor.Formats
                             sw.WriteLine(indent + "fadeout(" + arg1 + ", time[" + arg2 + "]);");
                             break;
 
+                        case 0x0B:
+                            // Wait for Input
+                            arg1 = parseTime(fileData.Skip(pointer + 2).Take(2).ToArray());
+                            pointer = nexptr;
+                            sw.WriteLine(indent + "input.wait(time[" + arg2 + "]);");
+                            break;
+
+                        case 0x0C:
+                            // Shake Screen
+                            arg1 = parseTime(fileData.Skip(pointer + 2).Take(2).ToArray());
+                            arg2 = parseTime(fileData.Skip(pointer + 4).Take(2).ToArray());
+                            pointer = nexptr;
+                            sw.WriteLine(indent + "system.shake(intensity [" + arg1 + ", time[" + arg2 + "]);");
+                            break;
+
+                     /* case 0x0D:
+                            // Unknown Opcode. Not used in any retail script.
+                            break; */
+
+                     /* case 0x0E:
+                            // NEEDS PARSING
+                            break; */
+
+                     /* case 0x0F, 0x10:
+                            // Unknown Opcode. Not used in any retail script.
+                            break; */
+
+                     /* case 0x11:
+                            // NEEDS PARSING
+                            break; */
+
                         case 0x12:
                             // Call Script
                             arg1 = Convert.ToString(fileData[pointer + 2] | (fileData[pointer + 3] << 8) | (fileData[pointer + 4] << 16)).PadLeft(8, '0');
@@ -146,12 +177,64 @@ namespace DisgaeaScriptEditor.Formats
                             sw.WriteLine(indent + "load.script(" + arg1 + ");");
                             break;
 
+                     /* case 0x13, 0x14:
+                            // Unknown Opcode. Not used in any retail script.
+                            break; */
+
                         case 0x15:
                             // Load Map
                             arg1 = parseInt16(fileData.Skip(pointer + 2).Take(2).ToArray());
                             pointer = nexptr;
                             sw.WriteLine(indent + "load.map(" + arg1 + ");");
                             break;
+
+                        case 0x16:
+                            // Load Item World Map
+                            arg1 = parseInt16(fileData.Skip(pointer + 2).Take(2).ToArray());
+                            pointer = nexptr;
+                            sw.WriteLine(indent + "load.itemworld(" + arg1 + ");");
+                            break;
+
+                     /* case 0x17:
+                            // NEEDS PARSING
+                            break; */
+
+                        case 0x18:
+                            // Reset Camera and Player Position
+                            pointer = nexptr;
+                            sw.WriteLine(indent + "system.resetmap();");
+                            break;
+
+                     /* case 0x19, 0x1A:
+                            // Unknown Opcode. Not used in any retail script.
+                            break; */
+
+                        case 0x1B:
+                            // Set Background Gradient
+                            arg1 = parseTime(fileData.Skip(pointer + 2).Take(2).ToArray());
+                            arg2 = Convert.ToInt32(fileData[pointer + 6] | (fileData[pointer + 5] << 8) | (fileData[pointer + 4] << 16)).ToString("X2").PadLeft(6, '0');
+                            arg3 = Convert.ToInt32(fileData[pointer + 9] | (fileData[pointer + 8] << 8) | (fileData[pointer + 7] << 16)).ToString("X2").PadLeft(6, '0');
+                            arg4 = Convert.ToInt32(fileData[pointer + 12] | (fileData[pointer + 11] << 8) | (fileData[pointer + 10] << 16)).ToString("X2").PadLeft(6, '0');
+                            arg5 = Convert.ToInt32(fileData[pointer + 15] | (fileData[pointer + 14] << 8) | (fileData[pointer + 13] << 16)).ToString("X2").PadLeft(6, '0');
+                            pointer = nexptr;
+                            sw.WriteLine(indent + "set.gradient(time[" + arg1 + "], topleft[" + arg2 + "], topright[" + arg3 + "], bottomleft[" + arg4 + "], bottomright[" + arg5 + "]);");
+                            break;
+
+                     /* case 0x1C:
+                            // NEEDS PARSING
+                            break; */
+
+                     /* case 0x1D, 0x1E, 0x1F, 0x20, 0x21, 0x22:
+                            // Unknown Opcode. Not used in any retail script.
+                            break; */
+
+                     /* case 0x23, 0x24:
+                            // NEEDS PARSING
+                            break; */
+
+                     /* case 0x25, 0x26:
+                            // Unknown Opcode. Not used in any retail script.
+                            break; */
 
                         case 0x27:
                             // Move Cursor
@@ -178,6 +261,10 @@ namespace DisgaeaScriptEditor.Formats
                             sw.WriteLine(indent + "camera.zoom(time[" + arg1 + "], " + arg2 + ");");
                             break;
 
+                     /* case 0x2A:
+                            // NEEDS PARSING
+                            break; */
+
                         case 0x2B:
                             // Camera Pitch
                             arg1 = parseTime(fileData.Skip(pointer + 2).Take(2).ToArray());
@@ -196,12 +283,38 @@ namespace DisgaeaScriptEditor.Formats
                             sw.WriteLine(indent + "camera.pan(" + arg1 + ", x[" + arg2 + "], y[" + arg3 + "], time[" + arg4 + "]);");
                             break;
 
+                     /* case 0x2D, 0x2E:
+                            // NEEDS PARSING
+                            break; */
+
+                     /* case 0x2F, 0x30, 0x31:
+                            // Unknown Opcode. Not used in any retail script.
+                            break; */
+
                         case 0x32:
                             // Print String
                             arg1 = parseString();
                             pointer = nexptr;
                             sw.WriteLine(indent + "print(\"" + arg1 + "\");");
                             break;
+
+                     /* case 0x33:
+                            // Unknown Opcode. Not used in any retail script.
+                            break; */
+
+                     case 0x34:
+                            // Wait for Input (Indefinitely)
+                            pointer = nexptr;
+                            sw.WriteLine(indent + "system.wait();");
+                            break;
+
+                     /* case 0x35, 0x36:
+                            // Unknown Opcode. Not used in any retail script.
+                            break; */
+
+                     /* case 0x37, 0x38:
+                            // NEEDS PARSING
+                            break; */
 
                         case 0x39:
                             // Set Background
@@ -218,6 +331,18 @@ namespace DisgaeaScriptEditor.Formats
                             pointer = nexptr;
                             sw.WriteLine(indent + "system.bg(" + arg1 + ");");
                             break;
+
+                     /* case 0x3B, 0x3C, 0x3D, 0x3E, 0x3F, 0x40, 0x41, 0x42, 0x43, 0x44, 0x45:
+                            // Unknown Opcode. Not used in any retail script.
+                            break; */
+
+                     /* case 0x46:
+                            // NEEDS PARSING
+                            break; */
+
+                     /* case 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D:
+                            // Unknown Opcode. Not used in any retail script.
+                            break; */
 
                         case 0x4E:
                             // Spawn Actor
@@ -250,12 +375,24 @@ namespace DisgaeaScriptEditor.Formats
                             sw.WriteLine(indent + "get.item(" + arg1 + ", " + arg2 + ");");
                             break;
 
+                     /* case 0x52:
+                            // Unknown Opcode. Not used in any retail script.
+                            break; */
+
                         case 0x53:
                             // Input Control
                             arg1 = parseInt16(fileData.Skip(pointer + 2).Take(2).ToArray());
                             pointer = nexptr;
                             sw.WriteLine(indent + "input(" + arg1 + ");");
                             break;
+
+                     /* case 0x54, 0x55:
+                            // NEEDS PARSING
+                            break; */
+
+                     /* case 0x56, 0x57, 0x58, 0x59:
+                            // Unknown Opcode. Not used in any retail script.
+                            break; */
 
                         case 0x5A:
                             // Game State
@@ -264,6 +401,30 @@ namespace DisgaeaScriptEditor.Formats
                             pointer = nexptr;
                             sw.WriteLine(indent + "set.state(" + arg1 + ", " + arg2 + ");");
                             break;
+
+                     /* case 0x5B:
+                            // Unknown Opcode. Not used in any retail script.
+                            break; */
+
+                     /* case 0x5C:
+                            // NEEDS PARSING
+                            break; */
+
+                     /* case 0x5D, 0x5E, 0x5F, 0x60:
+                            // Unknown Opcode. Not used in any retail script.
+                            break; */
+
+                     /* case 0x61:
+                            // NEEDS PARSING
+                            break; */
+
+                     /* case 0x62, 0x63:
+                            // Unknown Opcode. Not used in any retail script.
+                            break; */
+
+                     /* case 0x64:
+                            // NEEDS PARSING
+                            break; */
 
                         case 0x65:
                             // Set Position
@@ -286,13 +447,21 @@ namespace DisgaeaScriptEditor.Formats
                             sw.WriteLine(indent + "set.sprite(actor[" + arg1 + "], " + arg2 + ", " + arg3 + ");");
                             break;
 
-                        case 0x6B:
+                        case 0x67:
                             // Set Animation
                             arg1 = fileData[pointer + 2].ToString();
-                            arg2 = parseInt16(fileData.Skip(pointer + 3).Take(2).ToArray());
+                            arg2 = parseInt32(fileData.Skip(pointer + 3).Take(4).ToArray());
                             pointer = nexptr;
                             sw.WriteLine(indent + "set.anim(actor[" + arg1 + "], " + arg2 + ");");
                             break;
+
+                     /* case 0x68, 0x69, 0x6A, 0x6B:
+                            // NEEDS PARSING
+                            break; */
+
+                     /* case 0x6C:
+                            // Unknown Opcode. Not used in any retail script.
+                            break; */
 
                         case 0x6D:
                             // Spawn NPC
@@ -313,6 +482,10 @@ namespace DisgaeaScriptEditor.Formats
                             sw.WriteLine(indent + "spawn.prop(actor[" + arg1 + "], " + arg2 + ", " + arg3 + ");");
                             break;
 
+                     /* case 0x6F, 0x70, 0x71, 0x72:
+                            // NEEDS PARSING
+                            break; */
+
                         case 0x73:
                             // Set Rotation
                             arg1 = fileData[pointer + 2].ToString();
@@ -321,6 +494,18 @@ namespace DisgaeaScriptEditor.Formats
                             pointer = nexptr;
                             sw.WriteLine(indent + "set.rot(actor[" + arg1 + "], time[" + arg2 + "], " + arg3 + ");");
                             break;
+
+                     /* case 0x74:
+                            // NEEDS PARSING
+                            break; */
+
+                     /* case 0x75, 0x76:
+                            // Unknown Opcode. Not used in any retail script.
+                            break; */
+
+                     /* case 0x77, 0x78, 0x79:
+                            // NEEDS PARSING
+                            break; */
 
                         case 0x7A:
                             // Set Color \ Alpha
@@ -335,6 +520,46 @@ namespace DisgaeaScriptEditor.Formats
                             sw.WriteLine(indent + "set.filter(actor[" + arg1 + "], " + arg2 + ", red[" + arg3 + "], blue[" + arg4 + "], green[" + arg5 + "], alpha[" + arg6 + "], " + arg7 + ");");
                             break;
 
+                     /* case 0x7B:
+                            // NEEDS PARSING
+                            break; */
+
+                     /* case 0x7C, 0x7D, 0x7E, 0x7F, 0x80, 0x81:
+                            // Unknown Opcode. Not used in any retail script.
+                            break; */
+
+                     /* case 0x82:
+                            // NEEDS PARSING
+                            break; */
+
+                        case 0x83:
+                            // Play Voice Over
+                            arg1 = parseInt16(fileData.Skip(pointer + 2).Take(2).ToArray());
+                            arg2 = fileData[pointer + 4].ToString();
+                            pointer = nexptr;
+                            sw.WriteLine(indent + "sound.voice(id[" + arg1 + "], volume[" + arg2 + "]);");
+                            break;
+
+                     /* case 0x84, 0x85, 0x86:
+                            // Unknown Opcode. Not used in any retail script.
+                            break; */
+
+                     /* case 0x87:
+                            // NEEDS PARSING
+                            break; */
+
+                        case 0x88:
+                            // Play Battle Cry
+                            arg1 = parseInt16(fileData.Skip(pointer + 2).Take(2).ToArray());
+                            arg2 = fileData[pointer + 4].ToString();
+                            pointer = nexptr;
+                            sw.WriteLine(indent + "sound.battle(id[" + arg1 + "], volume[" + arg2 + "]);");
+                            break;
+
+                     /* case 0x89, 0x8A, 0x8B:
+                            // Unknown Opcode. Not used in any retail script.
+                            break; */
+
                         case 0x8C:
                             // Play SFX
                             arg1 = parseInt16(fileData.Skip(pointer + 2).Take(2).ToArray());
@@ -344,12 +569,36 @@ namespace DisgaeaScriptEditor.Formats
                             sw.WriteLine(indent + "sound.sfx(id[" + arg1 + "], " + arg2 + ", " + arg3 + ");");
                             break;
 
+                     /* case 0x8D:
+                            // NEEDS PARSING
+                            break; */
+
+                     /* case 0x8E, 0x8F, 0x90:
+                            // Unknown Opcode. Not used in any retail script.
+                            break; */
+
                         case 0x91:
                             // Play BGM
                             arg1 = parseInt16(fileData.Skip(pointer + 2).Take(2).ToArray());
                             pointer = nexptr;
                             sw.WriteLine(indent + "sound.bgm(id[" + arg1 + "]);");
                             break;
+
+                        case 0x92:
+                            // Adjust BGM Volume
+                            arg1 = fileData[pointer + 2].ToString();
+                            arg2 = parseTime(fileData.Skip(pointer + 2).Take(2).ToArray());
+                            pointer = nexptr;
+                            sw.WriteLine(indent + "sound.bgmv(volume[" + arg1 + "], time[" + arg2 + "]);");
+                            break;
+
+                     /* case 0x93, 0x94, 0x95:
+                            // NEEDS PARSING
+                            break; */
+
+                     /* case 0x96, 0x97:
+                            // Unknown Opcode. Not used in any retail script.
+                            break; */
 
                         case 0x98:
                             // Set Talk ID
@@ -359,6 +608,11 @@ namespace DisgaeaScriptEditor.Formats
                             pointer = nexptr;
                             sw.WriteLine(indent + "set.talk(actor[" + arg1 + "], " + arg2 + ", " + arg3 + ");");
                             break;
+
+                     /* case 0x99, 0x9A, 0x9B, 0x9C, 0x9D, 0x9E, 0x9F, 0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF,
+                             0xB0, 0xB1, 0xB2, 0xB3, 0xB4, 0xB5, 0xB7, 0xB8, 0xB9, 0xBA, 0xBB, 0xBC, 0xBD, 0xBE, 0xBF, 0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7:
+                            // Unknown Opcode. Not used in any retail script.
+                            break; */
 
                         case 0xC8:
                             // Hardcoded Handler
@@ -378,6 +632,10 @@ namespace DisgaeaScriptEditor.Formats
                             sw.WriteLine(indent + "load.menu(" + arg1 + ");");
                             break;
 
+                     /* case 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF:
+                            // NEEDS PARSING
+                            break; */
+
                         case 0xD0:
                             // Unknown Opcode 0xD0
                             unk = opcode.ToString("X2");
@@ -386,12 +644,24 @@ namespace DisgaeaScriptEditor.Formats
                             sw.WriteLine(indent + "unknown" + "(op[" + unk + "], args[" + "4" + "], " + arg1 + ");");
                             break;
 
+                     /* case 0xD1:
+                            // NEEDS PARSING
+                            break; */
+
+                     /* case 0xD2, 0xD3, 0xD4, 0xD5, 0xD6, 0xD7, 0xD8, 0xD9, 0xDA, 0xDB, 0xDC:
+                            // Unknown Opcode. Not used in any retail script.
+                            break; */
+
                         case 0xDD:
                             // Print String for DS Version (Top Screen)
                             arg1 = parseString();
                             pointer = nexptr;
                             sw.WriteLine(indent + "print.ds(\"" + arg1 + "\");");
                             break;
+
+                     /* case 0xDE, 0xDF, 0xF0, 0xF1, 0xF2, 0xF3, 0xF4, 0xF5, 0xF6, 0xF7, 0xF8, 0xF9, 0xFA, 0xFB, 0xFC, 0xFD, 0xFE, 0xFF:
+                            // Unknown Opcode. Not used in any retail script.
+                            break; */
 
                         default:
                             // Unknown Opcode
